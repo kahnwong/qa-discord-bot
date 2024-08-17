2c5a4288e89376a1572d4f50661ced6c67a378d2
Karn Wong <karn@karnwong.me>
2024-08-17 11:53:34 +0700

    integrate with qa-api

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.22.5
 
 require (
 	github.com/bwmarrin/discordgo v0.28.1
+	github.com/carlmjohnson/requests v0.24.2
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/rs/zerolog v1.33.0
 )
@@ -12,6 +14,7 @@ require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/carlmjohnson/requests"
+	"github.com/google/uuid"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/rs/zerolog/log"
 )
 
 var (
 	discordAuthenticationToken = os.Getenv("DISCORD_AUTHENTICATION_TOKEN")
+	qaApiEndpoint              = os.Getenv("QA_API_ENDPOINT")
+	qaApiKey                   = os.Getenv("QA_API_KEY")
 )
+
+type submitRequest struct {
+	RequestID string `json:"request_id"`
+	Query     string `json:"query"`
+}
+
+type submitResponse struct {
+	RequestID string `json:"request_id"`
+	Query     string `json:"query"`
+	Response  string `json:"response"`
+}
 
 func handleRequest(s *discordgo.Session, m *discordgo.MessageCreate) {
 	command := "!request"
@@ -28,17 +45,42 @@ func handleRequest(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	// main
-	question := strings.Replace(m.Content, command, "", 1)
+	query := strings.Replace(m.Content, command, "", 1)
 
-	if question != "" {
+	var response submitResponse
+	if query != "" {
 		log.Info().
 			Str("author", m.Author.Username).
 			Str("channel", m.Message.ChannelID).
-			Msg(question)
+			Msg("Query received")
 
-		response := question
+		id := uuid.New()
 
-		_, err := s.ChannelMessageSend(m.ChannelID, response)
+		body := submitRequest{
+			RequestID: id.String(),
+			Query:     query,
+		}
+
+		err := requests.
+			URL(qaApiEndpoint).
+			Method(http.MethodPost).
+			Path("submit").
+			BodyJSON(body).
+			Cookie("access_token", qaApiKey).
+			ToJSON(&response).
+			Fetch(context.Background())
+
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to send request to qa-api")
+		}
+
+		log.Info().
+			Str("author", m.Author.Username).
+			Str("channel", m.Message.ChannelID).
+			Str("response", response.Response).
+			Msg(query)
+
+		_, err = s.ChannelMessageSend(m.ChannelID, response.Response)
 		if err != nil {
 			log.Error().Err(err).Msg("Error sending message")
 		}
